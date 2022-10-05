120faa34a5568273e5fb292f64fbe81dc401dfa2
Nikromes <Nikro99@mail.ru>
2022-10-05 13:45:20 +0300

    рабочая версия

---
+++ Steam_sell_OK_button_clicker.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Steam OK button clicker
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       Nikromes
+// @match        https://steamcommunity.com/**/**/inventory/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Steam OK button clicker - ready');
+    const marketSellDialog = document.getElementById('market_sell_dialog');
+    const buttonsBox = document.getElementById('market_sell_dialog_confirm_buttons');
+    const okButton = document.getElementById('market_sell_dialog_ok');
+    const throbber = document.getElementById('market_sell_dialog_throbber');
+    marketSellDialog.style.transition = 'none';
+    setInterval(() => {
+        if (marketSellDialog.style.display !== 'none' && buttonsBox.style.overflow === 'visible' && buttonsBox.style.display !== 'none' && throbber.style.display === 'none') {
+            console.log('OK button visible, clicking...');
+            okButton.click();
+        }
+    }, 300);
+})();
---
+++ readme.md
@@ -0,0 +1,3 @@
+Auto click "OK" button on inventory sell page
+
+Requires Greasemonkey on Firefox and Tampermonkey on Chrome.
